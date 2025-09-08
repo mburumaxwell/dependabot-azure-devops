100c273330bb05b410be790d7779ea592f6eafb6
Maxwell Weru <1645026+mburumaxwell@users.noreply.github.com>
2025-09-08 13:52:38 +0100

    Add vercel analytics and speed insights

--- apps/website/src/app/layout.tsx
+++ apps/website/src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 
@@ -12,6 +14,8 @@ export default function Layout({ children }: LayoutProps<'/'>) {
     <html lang='en' className={inter.className} suppressHydrationWarning>
       <body className='flex flex-col min-h-screen'>
         <RootProvider>{children}</RootProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
