a7e0add3bf0205d30fd68424704e1abc1e6d34b4
Maxwell Weru <1645026+mburumaxwell@users.noreply.github.com>
2025-09-11 14:22:02 +0100

    Only report to App Insights in production

--- apps/dashboard/src/components/provider.tsx
+++ apps/dashboard/src/components/provider.tsx
@@ -1,13 +1,14 @@
 'use client';
 
+import { environment } from '@paklo/cli/environment';
 import type { ReactNode } from 'react';
-
 import { ApplicationInsightsProvider } from './app-insights';
 import { TooltipProvider } from './ui/tooltip';
 
 export function Provider({ children }: { children: ReactNode }) {
+  const connectionString = environment.production ? process.env.NEXT_PUBLIC_APP_INSIGHTS_CONNECTION_STRING : undefined;
   return (
-    <ApplicationInsightsProvider connectionString={process.env.NEXT_PUBLIC_APP_INSIGHTS_CONNECTION_STRING}>
+    <ApplicationInsightsProvider connectionString={connectionString}>
       <TooltipProvider>{children}</TooltipProvider>
     </ApplicationInsightsProvider>
   );
--- apps/website/src/instrumentation.ts
+++ apps/website/src/instrumentation.ts
@@ -1,12 +1,12 @@
 import type { SpanExporter } from '@opentelemetry/sdk-trace-base';
+import { environment } from '@paklo/cli/environment';
 import { registerOTel } from '@vercel/otel';
 
 export async function register() {
   let traceExporter: SpanExporter | undefined;
 
-  const isVercelDeployment = Boolean(process.env.VERCEL_DEPLOYMENT_ID);
-  // TODO: remove this once CS is fixed
-  if (process.env.NEXT_PUBLIC_APP_INSIGHTS_CONNECTION_STRING) {
+  if (environment.production) {
+    const isVercelDeployment = Boolean(process.env.VERCEL_DEPLOYMENT_ID);
     if (!isVercelDeployment && process.env.NEXT_RUNTIME === 'nodejs') {
       const { AzureMonitorTraceExporter } = await import('@azure/monitor-opentelemetry-exporter');
       traceExporter = new AzureMonitorTraceExporter({
