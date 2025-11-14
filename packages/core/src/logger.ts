56261052f863b8a48b35bcf9332056919f0372ce
Maxwell Weru <1645026+mburumaxwell@users.noreply.github.com>
2025-11-14 00:04:33 +0000

    Fix formatting issue

--- packages/core/src/logger.ts
+++ packages/core/src/logger.ts
@@ -5,6 +5,9 @@ import { environment } from '@/environment';
 
 const stream = pretty({ colorize: true, ignore: 'pid,hostname' });
 
-export const logger = pino({
-  level: process.env.LOG_LEVEL || (environment.production ? 'warn' : 'debug'),
-}, stream);
+export const logger = pino(
+  {
+    level: process.env.LOG_LEVEL || (environment.production ? 'warn' : 'debug'),
+  },
+  stream,
+);
