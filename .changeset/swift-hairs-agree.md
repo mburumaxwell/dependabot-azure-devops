45e845638b45fc8906de4cfbc928072d629430a2
Maxwell Weru <1645026+mburumaxwell@users.noreply.github.com>
2025-11-28 15:23:14 +0000

    Refactor logger implementation to support customizable options and multiple output streams

---
+++ .changeset/swift-hairs-agree.md
@@ -0,0 +1,6 @@
+---
+"@paklo/core": patch
+"@paklo/cli": patch
+---
+
+Refactor logger implementation to support customizable options and multiple output streams
--- packages/core/src/logger.ts
+++ packages/core/src/logger.ts
@@ -1,11 +1,88 @@
-import pino from 'pino';
+import pino, { type DestinationStream } from 'pino';
 import pretty from 'pino-pretty';
 
-const stream = pretty({ colorize: true, ignore: 'pid,hostname' });
+export type LoggerCreateOptions = {
+  /**
+   * Log level
+   * Only set this if you must override the default log level logic from env variables
+   */
+  level?: 'fatal' | 'error' | 'warn' | 'info' | 'debug' | 'trace';
 
-export const logger = pino(
-  {
-    level: process.env.LOG_LEVEL || (process.env.NODE_ENV === 'production' ? 'warn' : 'debug'),
-  },
-  stream,
-);
+  /**
+   * Whether to include time stamps in log entries
+   * @default false
+   */
+  timestamp?: boolean;
+
+  /** Options for pretty printing */
+  pretty?: {
+    /**
+     * Whether to enable pretty printing
+     * @default true
+     */
+    enable?: boolean;
+
+    /**
+     * Whether to colorize the pretty printed output
+     * @default true
+     */
+    colorize?: boolean;
+
+    /**
+     * Whether to include the log level in pretty printed output
+     * @default false
+     */
+    includeLevel?: boolean;
+  };
+
+  /**
+   * Additional streams, if any, to write logs to.
+   * This is in addition to any pretty printing stream configured.
+   * Useful for adding things like OpenTelemetry streams.
+   */
+  streams?: DestinationStream[];
+};
+
+/**
+ * Creates a new logger with the given options.
+ * @param options - Logger creation options
+ * @returns A logger instance
+ */
+export function create(options: LoggerCreateOptions = {}) {
+  const {
+    level,
+    timestamp = false,
+    pretty: {
+      // options for pretty printing
+      enable: prettyEnable = true,
+      colorize: prettyColorize = true,
+      includeLevel: prettyIncludeLevel = false,
+    } = {},
+    streams = [],
+  } = options;
+
+  // configure pretty printing stream, if enabled
+  const prettyStream = prettyEnable
+    ? pretty({
+        colorize: prettyColorize,
+        ignore: 'pid,hostname',
+        customPrettifiers: prettyIncludeLevel ? undefined : { level: () => '' },
+      })
+    : undefined;
+
+  // create and return the logger
+  return pino(
+    {
+      level: level || process.env.LOG_LEVEL || (process.env.NODE_ENV === 'production' ? 'warn' : 'debug'),
+      timestamp,
+    },
+    pino.multistream([
+      // add streams conditionally
+      ...(prettyStream ? [prettyStream] : []),
+      ...streams,
+    ]),
+  );
+}
+
+/** Default logger instance */
+export const logger = create();
