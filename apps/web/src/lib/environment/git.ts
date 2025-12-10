e53157dee09171b415f47e4e4eebbd6f048c6684
Maxwell Weru <1645026+mburumaxwell@users.noreply.github.com>
2025-12-10 12:32:28 +0000

    Use DEPLOY_GIT_* envs instead of GITHUB_* so that tests stop failing

--- apps/web/src/lib/environment/git.ts
+++ apps/web/src/lib/environment/git.ts
@@ -1,27 +1,25 @@
 /**
  * Retrieves the name of the current Git branch from the environment variables or from Git itself.
  * The priority order for retrieving the branch name is as follows:
- * 1. `process.env.GITHUB_REF_NAME`
+ * 1. `process.env.DEPLOY_GIT_COMMIT_REF`
  * 2. `process.env.VERCEL_GIT_COMMIT_REF`
  * 4. Retrieve the branch name from Git using the `getBranchFromGit` function.
  * @returns The name of the current Git branch, or undefined if it cannot be determined.
  */
 export function getBranch(): string | undefined {
-  // GITHUB_REF_NAME may change on every build and we do not want the turbo cache to be invalidated on every build
-  return process.env.GITHUB_REF_NAME || process.env.VERCEL_GIT_COMMIT_REF || getBranchFromGit();
+  return process.env.DEPLOY_GIT_COMMIT_REF || process.env.VERCEL_GIT_COMMIT_REF || getBranchFromGit();
 }
 
 /**
  * Retrieves the Git SHA (commit hash) from the environment variables or from Git itself.
  * The priority order for retrieving the Git SHA is as follows:
- * 1. `process.env.GITHUB_SHA`
+ * 1. `process.env.DEPLOY_GIT_COMMIT_SHA`
  * 2. `process.env.VERCEL_GIT_COMMIT_SHA`
  * 4. Retrieve the SHA from Git using the `getShaFromGit` function.
  * @returns The Git SHA (commit hash) if available, otherwise `undefined`.
  */
 export function getSha(): string | undefined {
-  // GITHUB_SHA changes on every build and we do not want the turbo cache to be invalidated on every build
-  return process?.env.GITHUB_SHA || process?.env.VERCEL_GIT_COMMIT_SHA || getShaFromGit();
+  return process?.env.DEPLOY_GIT_COMMIT_SHA || process?.env.VERCEL_GIT_COMMIT_SHA || getShaFromGit();
 }
 
 /**
