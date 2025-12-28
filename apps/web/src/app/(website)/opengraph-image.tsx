85e0bc8c5321c42de90f1dbeb143ad1729137e37
Maxwell Weru <1645026+mburumaxwell@users.noreply.github.com>
2025-12-28 02:16:38 +0000

    Fix warning for unprocessed classes

--- apps/web/src/app/(website)/opengraph-image.tsx
+++ apps/web/src/app/(website)/opengraph-image.tsx
@@ -8,9 +8,9 @@ export const size = { width: 1200, height: 630 };
 export default async function Image() {
   return new ImageResponse(
     <div
-      // tw='gap-[60px]/ or tw='gap-15' do not work
+      // tw='gap-[60px] or tw='gap-15' do not work
       style={{ gap: '60px' }}
-      tw='h-full w-full flex flex-row items-center justify-center font-[Inter_Bold] bg-[#0a0a0a] p-20'
+      tw='h-full w-full flex flex-row items-center justify-center bg-[#0a0a0a] p-20'
     >
       <div tw='flex items-center justify-center'>
         <PakloLogo width={280} height={280} fill='#2E6417' />
@@ -18,7 +18,11 @@ export default async function Image() {
 
       <div tw='w-1 h-88 bg-[#2E6417] opacity-30' />
 
-      <div tw='flex flex-col justify-center gap-5'>
+      <div
+        // tw='gap-[20px] or tw='gap-5' do not work
+        style={{ gap: '20px' }}
+        tw='flex flex-col justify-center'
+      >
         <p tw='text-6xl font-bold text-white tracking-tight'>{config.title}</p>
         <p tw='text-4xl text-gray-400 max-w-xl leading-snug'>{config.description}</p>
         <p tw='text-gray-400'>{config.domain}</p>
