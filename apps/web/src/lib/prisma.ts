cb22054fe238e4108403ef07a2e3b90757e2ce44
Maxwell Weru <1645026+mburumaxwell@users.noreply.github.com>
2026-01-08 17:47:31 +0000

    Upgrade to prisma 7

--- apps/web/src/lib/prisma.ts
+++ apps/web/src/lib/prisma.ts
@@ -1,10 +1,12 @@
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@/../.generated/prisma/client';
 
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
 };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({});
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 export type {
   Feedback,
@@ -21,14 +23,5 @@ export type {
 } from '@/../.generated/prisma/client';
 
 export { Prisma, PrismaClient } from '@/../.generated/prisma/client';
-
-export type {
-  InputJsonArray,
-  InputJsonObject,
-  InputJsonValue,
-  JsonArray,
-  JsonObject,
-  JsonValue,
-} from '@/../.generated/prisma/runtime/library';
 
 if (process.env.NODE_ENV === 'development') globalForPrisma.prisma = prisma;
